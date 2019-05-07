3f9558ad5a337ca8346f90b3f7abb73425c9244a
Sheila Adr <siladr2019@gmail.com>
2019-05-07 13:45:22 +0200

    Comentario en variables y alternativas

---
+++ js/comentarios.js
@@ -0,0 +1,7 @@
+var miNombre = prompt("Introduce tu nombre: ");
+miNombre = miNombre.toLowerCase();
+if (miNombre == "pepito grillo") {
+    alert("Hola Pepito Grillo");
+} else {
+    alert("Hola " + miNombre + ", aunque no seas Pepito Grillo");
+}
