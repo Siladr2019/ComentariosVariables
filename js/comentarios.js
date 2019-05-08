e648d793aaca933d513f9821a5b19fb3784e57b6
Sheila Adr <siladr2019@gmail.com>
2019-05-08 09:51:10 +0200

    Añadido en js comentarios de una sola línea y más de una línea

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,7 +1,15 @@
 var miNombre = prompt("Introduce tu nombre: ");
-miNombre = miNombre.toLowerCase();
-if (miNombre == "pepito grillo") {
-    alert("Hola Pepito Grillo");
+miNombre = miNombre;
+if (miNombre.toLowerCase() == "pepito grillo") {
+    alert("Hola, Pepito Grillo");
 } else {
-    alert("Hola " + miNombre + ", aunque no seas Pepito Grillo");
+    alert("Hola, " + miNombre + " aunque no seas Pepito Grillo.");
 }
+
+// Esto es un comentario de una sola línea
+
+/*
+Esto es un comentario de más de una línea.
+Esto es la segunda línea del comentario.
+Es la tercera línea del comentario.
+*/
